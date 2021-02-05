2da0ecf5a84fa32430b04bc1c3e9ada0a1373ad1
Wangshuaishuai <w528764347@126.com>
2021-02-05 16:43:40 +0800

    feat: server,client

--- nteey/src/main/java/com/test/netty/config/two/test/TestService.java
+++ nteey/src/main/java/com/test/netty/config/two/test/TestService.java
@@ -6,13 +6,17 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.springframework.beans.factory.annotation.Value;
 
 public class TestService {
+    @Value("${netty.server-socket.url}")
+    private String socketUri;
     public static void main(String[] args) throws Exception{
 
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workGroup = new NioEventLoopGroup();
         try {
+
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(bossGroup,workGroup).channel(NioServerSocketChannel.class)
             .childHandler(new TestServerinitializer());
