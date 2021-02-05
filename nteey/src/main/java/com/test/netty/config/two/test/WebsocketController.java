80f30b32314c2cd21c9213e0b314dde6456c2d4f
Wangshuaishuai <w528764347@126.com>
2021-02-05 17:12:42 +0800

    feat: server,client

--- nteey/src/main/java/com/test/netty/config/two/test/WebsocketController.java
+++ nteey/src/main/java/com/test/netty/config/two/test/WebsocketController.java
@@ -1,6 +1,7 @@
 package com.test.netty.config.two.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebsocketController {
     @Autowired
     private ChannelSupervise channelSupervise;
+    @Value("${netty.server-socket.url}")
+    private String socketUri;
     @RequestMapping("sendToAll")
     public void sendToAll(String msg) {
+        System.out.println(socketUri);
         channelSupervise.sendToAll(msg);
     }
 
